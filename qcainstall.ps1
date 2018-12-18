6fc4a135f6fd9c8761493a234ed08e1cefed9a8d
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2018-12-18 13:55:28 +0530

    Add files via upload

--- qcainstall.ps1
+++ qcainstall.ps1
@@ -37,15 +37,16 @@ foreach ($ResourceGroup in $ResourceGroups)
         if ($_.StorageProfile.OsDisk.OsType -eq "Windows") {
             Write-Output "\nFor Windows Hosts"
             Write-Output $_.Name
-            Get-AzureStorageBlobContent -Blob WindowsQCA.ps1 -Container $ContainerName -Destination qcainstall.ps1 -Context $StorageContext
-            #wget "https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/WindowsQCA.ps1" -UseBasicParsing -outfile qcainstall.ps1
+            Get-AzureStorageBlobContent -Blob WindowsQCA.ps1 -Container $ContainerName -Destination qcainstall.ps1 -Context $StorageContext -Confirm:$false
+            #wget "https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/WindowsQCA.ps1" -UseBasicParsing -outfile qcainstall.ps1 # if script is stored in public container
             Invoke-AzureRmVMRunCommand -ResourceGroupName $ResourceGroup.ResourceGroupName -VMName $_.Name -CommandId 'RunPowerShellScript' -ScriptPath qcainstall.ps1 -Parameter @{"ActivationId" = "e84764e1-e219-4b59-8586-e781d3f621dd";"CustomerId" = "8a72be28-4fe4-7b79-838d-b5672cd680ab"} -Confirm:$false
         }
         else {
-        #    Write-Output "\nFor Linux Hosts"
-        #    Write-Output $_.Name
-        #    wget "https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/LinuxQCA.sh" -UseBasicParsing -outfile qcainstall.sh
-        #    Invoke-AzureRmVMRunCommand -ResourceGroupName $ResourceGroup.ResourceGroupName -VMName $_.Name -CommandId 'RunShellScript' -ScriptPath .\qcainstall.sh -Parameter @{"a"="e84764e1-e219-4b59-8586-e781d3f621dd"; "b"="8a72be28-4fe4-7b79-838d-b5672cd680ab"; "c"="https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/qualys-cloud-agent.x86_64_qg2.rpm"; "d"="https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/qualys-cloud-agent.x86_64_qg2.deb"} -Confirm:$false    
+            Write-Output "\nFor Linux Hosts"
+            Write-Output $_.Name
+            Get-AzureStorageBlobContent -Blob LinuxQCA.sh -Container $ContainerName -Destination qcainstall.sh -Context $StorageContext -Confirm:$false
+            #wget "https://csg88d63f93fad0x471dx88f.blob.core.windows.net/containermiktest/LinuxQCA.sh" -UseBasicParsing -outfile qcainstall.sh # if script is stored in public container
+            Invoke-AzureRmVMRunCommand -ResourceGroupName $ResourceGroup.ResourceGroupName -VMName $_.Name -CommandId 'RunShellScript' -ScriptPath qcainstall.sh -Parameter @{"arg1"="e84764e1-e219-4b59-8586-e781d3f621dd"; "arg2"="8a72be28-4fe4-7b79-838d-b5672cd680ab"; "arg3"="https://testmikstrg.blob.core.windows.net/testmikconpub/qualys-cloud-agent.x86_64_qg2.rpm"; "arg4"="https://testmikstrg.blob.core.windows.net/testmikconpub/qualys-cloud-agent.x86_64_qg2.deb"} -Confirm:$false    
         }
     } 
 
